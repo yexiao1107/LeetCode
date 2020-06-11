175a65102beab42ca430e444cba0b8c8f9e568e5
yangxiao <921242428@qq.com>
2020-06-11 22:24:49 +0800

    反转整数

--- Int/reverseInteger.py
+++ Int/reverseInteger.py
@@ -21,14 +21,23 @@ def reverse(x):
     :type x: int
     :rtype: int
     """
-    if x > 0:
-        x = int(str(x)[::-1])
-    else:
-        x = -int(str(-x)[::-1])
-    if x > 2 ** 31 - 1 or x < -2 ** 31:
-        return 0
-    return x
+    INT_MAX = pow(2, 31) - 1
+    INT_MIN = - pow(2, 31)
+    rev = 0
+    while x != 0:
+        if x > 0:
+            pop = x % 10
+            x = x // 10
+        else:
+            pop = x % -10
+            x = -(x // -10)
+        if rev > INT_MAX / 10 or (rev == INT_MAX // 10 and pop > 7):
+            return 0
+        if rev < INT_MIN / 10 or (rev == INT_MIN // 10 and pop < -8):
+            return 0
+        rev = rev * 10 + pop
+    return rev
 
 
 if __name__ == "__main__":
-    print(reverse(123))
+    print(reverse(-123))
