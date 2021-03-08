4673405c056f27a9b88cb6ace4bd7650347dff3b
yangxiao <921242428@qq.com>
2021-03-08 23:21:36 +0800

    二叉搜索树

---
+++ Tree/700.二叉树中的搜索.py
@@ -0,0 +1,38 @@
+'''
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。
+返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+
+例如，
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和值: 2
+你应该返回如下子树:
+
+      2
+     / \
+    1   3
+'''
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root == None or root.val == val:
+            return root
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
