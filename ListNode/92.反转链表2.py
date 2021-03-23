cb136a7563adb5852d467966e7a40444766a9d31
yangxiao <921242428@qq.com>
2021-03-23 23:02:27 +0800

    更新反转链表

--- ListNode/92.反转链表2.py
+++ ListNode/92.反转链表2.py
@@ -39,3 +39,18 @@ class Solution:
     #     head.next.next = head
     #     head.next = None
     #     return last
+    def reverseBetween2(self, head: ListNode, left: int, right: int) -> ListNode:
+        # 设置 dummyNode 是这一类问题的一般做法
+        dummy_node = ListNode(-1)
+        dummy_node.next = head
+        pre = dummy_node
+        for _ in range(left - 1):
+            pre = pre.next
+
+        cur = pre.next
+        for _ in range(right - left):
+            next = cur.next
+            cur.next = next.next
+            next.next = pre.next
+            pre.next = next
+        return dummy_node.next
--- Tree/116.填充每个节点的下一个右侧节点指针.py
+++ Tree/116.填充每个节点的下一个右侧节点指针.py
@@ -8,7 +8,7 @@ struct Node {
   Node *next;
 }
 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，
-则将 next 指针设置为 NULL。初始状态下，所有 next 指针都被设置为 NULL。
+则将 next 指针设置为 NULL。初始状态下，所有next 指针都被设置为 NULL。
 
 进阶：
 
